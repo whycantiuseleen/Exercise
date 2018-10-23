463702ce19cde9e7263788f23f87a21c6c4dc1e2
thanhdoan <antoniedoan@gmail.com>
2018-10-23 11:29:13 +0800

    added methods in blockchain and minerApi files

--- blockchain.py
+++ blockchain.py
@@ -4,16 +4,30 @@ from ecdsa import SigningKey
 from merkletree import MerkleTree
 from transaction import Transaction
 from collections import OrderedDict
+from urllib.parse import urlparse
+
 
 class Blockchain:
     def __init__(self):
-        self.blockchain = []   
+        self.blockchain = []
         self.transactionpool = []
         self.currentindex = 0
-        
+
         #Genesis
         self.genesis_created = False
         self.genesis = self.create_first_block()
+
+        self.peers = set()
+
+    def register_peer(self, addr):
+        #add peer to list of peers
+        parsedUrl = urlparse(addr)
+        if parsedUrl.netloc:
+            self.peers.add(parsedUrl.netloc)
+        elif parsedUrl.path:
+            self.peers.add(parsedUrl.path)
+        else:
+            raise ValueError('Invalid URL')
 
     def validate_block(self,block):
         getlastblock = self.blockchain[-1][0]
@@ -21,10 +35,10 @@ class Blockchain:
             return True
         else:
             return False
-        
+
     def set_blockheader(self,block):
-        lastblock = self.blockchain[-1] 
-    
+        lastblock = self.blockchain[-1]
+
         # Set header values
         block.index  = len(self.blockchain) + 1
         block.pre_hash = lastblock[0].hash
@@ -37,10 +51,10 @@ class Blockchain:
             chain.append([block])
             print ("Adding Block " + str(block.index) + " to the chain at " + str(block.timestamp))
             print ("Hashed Data:", block.hash.hexdigest())
-            print ("Current Number of Block is " + str(len(self.blockchain)))  
-            
+            print ("Current Number of Block is " + str(len(self.blockchain)))
+
             self.transactionpool = []
-            
+
             rewardtxn = Transaction("ServerReward", minerpublickey, 100).to_json()
             self.transactionpool.append(rewardtxn)
             print ("Rewarded Miner "+str(minerpublickey) + " with 100 SUTDcoin\n")
@@ -53,7 +67,7 @@ class Blockchain:
             txn1 = [Transaction('genesis_sender','ServerReward',10000).to_json()]
             self.transactionpool.append(txn1)
             self.genesis = Block(txn1)
-            
+
             #genesis header - root, timestamp, prehash, nonce
             self.blockchain.append([self.genesis])
             self.genesis_created = True
@@ -61,24 +75,46 @@ class Blockchain:
             self.genesis.index = current_index
             self.currentindex = current_index
             print ("Genesis Block with index " + str(current_index) +" added at " + str(datetime.utcnow()) + " to the chain \n")
-        
+
         return self.genesis
-    
+
     def proof_of_work(self,block):
         while block.hash.hexdigest()[:4] != '0000':
             block.proof += 1
             block.hash_data()
-        
+
         print ("Validated proof: "+str(block.proof))
-        return True 
+        return True
 
     @property
     def last_block(self):
         lastblock = self.blockchain[-1]
         return lastblock
 
+    @classmethod
+    def is_valid_proof(cls, block, blockHash):
+        return (blockHash.startswith('0'* self.TARGET ) and blockHash == block.hashHeader())
+
+    @classmethod
+    def is_valid_chain(cls, chain):
+        prev_hash = '0'
+
+        for block in chain:
+            block_hash = block.hash
+
+            #remove hash and recompute using compute hash method
+            delattr(block, 'hash')
+
+            if not cls.is_valid_proof(block, block.hash) or prev_hash != block.prevHash:
+                return False
+                break
+
+            block.hash, prev_hash = block_hash, block_hash
+
+        return True
+
     def resolve(self):
-        peers = self.nodes
+        peers = self.peers
         longest_chain = None
         currentLen = len(self.chain)
 
@@ -96,19 +132,19 @@ class Blockchain:
                     longest_chain = chain
         #update chain to the longest one
         if longest_chain:
-            self.chain = longest_chain
+            self.blockchain = longest_chain
             return True
 
         return False
 
-   
+
 class Block:
     def __init__(self, txnlist):
         self.txnlist = txnlist
         #Header
-        # Set these values from blockchain 
+        # Set these values from blockchain
         self.pre_hash = None
-        self.index = None    
+        self.index = None
 
         # Set when block is created
         self.proof = 0
@@ -117,21 +153,21 @@ class Block:
 
         #Hash data
         self.hash = None
-        
+
 
     def hash_data(self):
         hashed = hashlib.sha256()
         hashed.update(str(self.pre_hash).encode('utf-8')+str(self.timestamp).encode('utf-8')+str(self.proof).encode('utf-8')+str(self.merkle_root).encode('utf-8'))
         self.hash = hashed
         return hashed
-    
+
 
     def build_tree(self):
         mt = MerkleTree(self.txnlist)
         buildtree= mt.build()
         merkleroot = mt.get_root()
         self.merkle_root = merkleroot
-       
+
         return merkleroot
 
     def get_header(self):
--- miner.py
+++ miner.py
@@ -8,14 +8,14 @@ import json
 class Miner:
     def __init__(self, blockchain):
         self.blockchain = blockchain
-    
+
         # wallet
         self.privatekey = SigningKey.generate(curve=NIST192p)
         self.publickey = self.privatekey.get_verifying_key()
 
         # Address book of txn
         self.addr = {}
-       
+
     def getAddrBalance(self,chain):
         addr = {}
         accountlist = list(addr.keys())
@@ -24,28 +24,28 @@ class Miner:
                 data = json.loads(transaction)
                 if data['sender'] in accountlist:
                     balance_tominus = addr.get(data['sender']) - data['amount']
-                    addr.update({str(data['sender']):balance_tominus})                    
+                    addr.update({str(data['sender']):balance_tominus})
                     if data['receiver'] in accountlist:
                         balance_toadd = addr.get(data['receiver']) + data['amount']
-                        addr.update({str(data['receiver']):balance_toadd}) 
+                        addr.update({str(data['receiver']):balance_toadd})
                     elif data['receiver'] not in accountlist:
                         accountlist.append(data['receiver'])
                         addr.update({str(data['receiver']):data['amount']})
-                     
-                else: 
+
+                else:
                     accountlist.append(data['sender'])
                     addr.update({str(data['sender']):-data['amount']})
-                    
+
                     if data['receiver'] in accountlist:
                         balance_toadd = addr.get(data['receiver']) + data['amount']
-                        addr.update({str(data['receiver']):balance_toadd})   
+                        addr.update({str(data['receiver']):balance_toadd})
                     elif data['receiver'] not in accountlist:
                         accountlist.append(data['receiver'])
                         addr.update({str(data['receiver']):data['amount']})
         # print (addr)
-        self.addr = addr   
+        self.addr = addr
         return addr
-    
+
     def check_transactions(self,transactionpool):
         # Check Signature
         # Check if the sender has enough coins to send
@@ -54,44 +54,26 @@ class Miner:
         accountlist = list(addrbk_checked.keys())
         for transaction in transactionpool:
             # Cost of transaction
-            pool_data = json.loads(transaction) 
+            pool_data = json.loads(transaction)
             cost = pool_data['amount']
             sender = pool_data['sender']
             # Amt that sender has
-            amt_onhand = addrbk_checked.get(sender) 
-            
-            if sender not in accountlist: 
+            amt_onhand = addrbk_checked.get(sender)
+
+            if sender not in accountlist:
                 # print (str(sender) + " not in account list, transaction rejected")
                 continue
 
-            else: 
+            else:
                 # print ('Amount on hand in addrbk', amt_onhand)
                 if amt_onhand > cost:
                     balance = amt_onhand - cost
                     addrbk_checked.update({pool_data['sender']:balance})
                     verifiedpool.append(transaction)
                     # print ("Transaction is valid\n")
-                
+
 
         return verifiedpool
-
-    def mine(self, currentchain):
-        self.getAddrBalance(currentchain)
-
-        # Blockchain obj
-        transactionpool = currentchain.transactionpool
-        # Check Transactions
-        verifiedpool = self.check_transactions(transactionpool)
-        
-        print ("\nMining Transaction, current number of block is: "+ str(len(currentchain.blockchain)))
-        # Create block object
-        newblock = Block(verifiedpool)
-        currentchain.set_blockheader(newblock)
-
-        proof = currentchain.proof_of_work(newblock)
-        print ("\nMiner " + str(self.publickey.to_string().hex())+" found new block")
-        return newblock
-
 
     def new_txn(self, recipient, amount):
         self.getAddrBalance(self.blockchain)
@@ -104,7 +86,27 @@ class Miner:
             print ("\nMiner "+str(senderkey)+" Added Transaction to transaction pool \n", self.blockchain.transactionpool)
         else:
             print ('\nMiner '+str(senderkey)+" does not have sufficient coins to send a transaction")
-        return None 
+        return None
+        
+    def mine(self, currentchain):
+        self.getAddrBalance(currentchain)
+
+        # Blockchain obj
+        transactionpool = currentchain.transactionpool
+        # Check Transactions
+        verifiedpool = self.check_transactions(transactionpool)
+
+        print ("\nMining Transaction, current number of block is: "+ str(len(currentchain.blockchain)))
+        # Create block object
+        newblock = Block(verifiedpool)
+        currentchain.set_blockheader(newblock)
+
+        proof = currentchain.proof_of_work(newblock)
+        print ("\nMiner " + str(self.publickey.to_string().hex())+" found new block")
+        return newblock
+
+
+
 
     def get_merklepath(self, txn):
         # Get Tree information from minernode
@@ -114,7 +116,7 @@ class Miner:
                 merkletree = MerkleTree(block[0].txnlist)
                 merkletree.build()
                 arrayTree = merkletree.tree
-                
+
                 # Get merkle path
                 proof = merkletree.get_proof(txn)
 
